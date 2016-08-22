08c35fd4024ab4b8fae6db50e0b2278441e12680
Ian Baum and Zach Baum <zach@>
2016-08-22 11:31:53 -0400

    add list append feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+
+  function Task(name, details) {
+    this.taskName = name;
+    this.taskDetails = details;
+  }
+
   $("#userInput").submit(function(event) {
     event.preventDefault();
-    
+
+    var taskName = $("#taskInput").val();
+    var taskDetails = $("#detailsInput").val();
+
+    var newTask = new Task(taskName, taskDetails);
+
+    $("#tasks").append("<li><span class='task'>" + newTask.taskName + "</span></li>")
   })
 });
